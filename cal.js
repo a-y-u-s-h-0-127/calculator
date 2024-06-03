2e9be57bd0969c9e3406a36dd83590028dccd625
AYUSH SINGH <ayushsinghascii0127@gmail.com>
2024-06-03 11:49:27 +0530

    update cal.js

--- cal.js
+++ cal.js
@@ -7,6 +7,7 @@ Array.from(buttons).forEach((button) => {
             document.querySelector('input').value = number;
         }
 
+
         else if (element.target.innerHTML === 'AC') {
             number = "";
             document.querySelector('input').value = number;
@@ -41,6 +42,37 @@ function percentage() {
 
 document.body.addEventListener('keydown', (event) => {
     let input = document.getElementById('calculator');
-    event.target.innerHTML;
-    input.value += event.key;
+    if (event.key == "0" || event.key == "1"
+        || event.key == "2" || event.key == "3"
+        || event.key == "4" || event.key == "5"
+        || event.key == "6" || event.key == "7"
+        || event.key == "8" || event.key == "9"
+        || event.key == "+" || event.key == "-"
+        || event.key == "*" || event.key == "/"
+        || event.key == "." || event.key == "%"
+        || event.key == "=" || event.key == " Enter"
+        || event.key == "backspace") {
+
+        event.target.innerHTML;
+        input.value += event.key;
+    }
+
+
+    else if (event.key === "Enter" || event.key === "=") {
+        try {
+            input.value = eval(input.value);
+        } catch (e) {
+            input.value = "Error";
+        }
+    }
+
+    else if (event.key === "Backspace") {
+        input.value = input.value.slice(0, -1);
+    }
+
+    else {
+        event.key = "";
+    }
 });
+
+
